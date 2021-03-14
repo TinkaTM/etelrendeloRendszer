94a627d70f6d867d57bda2112bb4b73766206bf7
TinkaTM <tinka.martin00@gmail.com>
2021-03-14 15:19:10 +0100

    Update Program.cs

--- FoodApp/Program.cs
+++ FoodApp/Program.cs
@@ -1,5 +1,9 @@
+using FoodApp.Data;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -9,18 +13,49 @@ using System.Threading.Tasks;
 
 namespace FoodApp
 {
+    /*   public class Program
+       {*/
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var dbContext = services.GetRequiredService<ApplicationDbContext>();
+                    //dbContext.Database.EnsureCreated(); //ensure db is created (created database can't be later updated using migrations!)
+                    dbContext.Database.Migrate(); //ensure pending migrations applied and db is created 
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+
+            host.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
     }
+    /* public static void Main(string[] args)
+     {
+         CreateHostBuilder(args).Build().Run();
+     }
+
+     public static IHostBuilder CreateHostBuilder(string[] args) =>
+         Host.CreateDefaultBuilder(args)
+             .ConfigureWebHostDefaults(webBuilder =>
+             {
+                 webBuilder.UseStartup<Startup>();
+             });
+ }*/
 }
