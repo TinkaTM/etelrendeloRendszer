2850a97f782dd8951b021436986b88ea9260a15a
NagyFelixNF <huntertelo25@gmail.com>
2021-04-07 15:18:35 +0200

    Rendelések Done
    Valaki amúgy olvassa ezeket?
    Kész a 2. mérföld kb. szépítgetni kéne rajta

---
+++ FoodApp/Controllers/RendelesEtteremController.cs
@@ -0,0 +1,62 @@
+﻿using FoodApp.Data;
+using FoodApp.Models;
+using FoodApp.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodApp.Controllers
+{
+    public class RendelesEtteremController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public RendelesEtteremController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Rendelesek()
+        {
+
+            List<RendelesEtteremViewModel> vms = new List<RendelesEtteremViewModel>();
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            var UserId = user.Id;
+            var Etteremid = _context.EtteremCim.Where(s => s.UserId == UserId).FirstOrDefault().ID;
+            var etelek = _context.RendelesDetail.Where(s => s.EtteremCimId == Etteremid).ToList();
+            if(etelek.Count > 0)
+            {
+                Dictionary<int, List<RendelesDetail>> dict = new Dictionary<int, List<RendelesDetail>>();
+                foreach (var etel in etelek)
+                {
+                    etel.Etlap = _context.Etlap.Find(etel.EtlapId);
+                    if (!dict.ContainsKey(etel.RendelesId)) dict.Add(etel.RendelesId, new List<RendelesDetail>());
+                    dict[etel.RendelesId].Add(etel);
+                }
+                foreach(var rendeles in dict)
+                {
+                    RendelesEtteremViewModel vm = new RendelesEtteremViewModel
+                    {
+                        RendelesAdatok = _context.Rendeles.Find(rendeles.Key),
+                        RendelesEtelek = rendeles.Value,
+                    };
+                    int total = 0;
+                    foreach(var etel in rendeles.Value)
+                    {
+                        int ar = etel.Etlap.Ar * etel.Darab;
+                        total = total + ar;
+                    }
+                    vm.RendelesTotal = total;
+                    vms.Add(vm);
+                }
+            }
+           
+            
+            return View(vms);
+        }
+    }
+}
---
+++ FoodApp/ViewModels/RendelesEtteremViewModel.cs
@@ -0,0 +1,15 @@
+﻿using FoodApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodApp.ViewModels
+{
+    public class RendelesEtteremViewModel
+    {
+        public Rendeles RendelesAdatok { get; set; }
+        public List<RendelesDetail> RendelesEtelek { get; set; }
+        public int RendelesTotal { get; set; }
+    }
+}
