f4bde00f7424f00cabbcfbe8f5012a5041301de3
TinkaTM <tinka.martin00@gmail.com>
2021-04-08 16:47:42 +0200

    Rendelés, címek
    Rendelésekhez hozzá lett adva, ha nincs rendelés éppen. Címekhez pedig az, hogy kötelező legyen őket megadni. nagyon jól működnek *wink*

--- FoodApp/Models/VendegCim.cs
+++ FoodApp/Models/VendegCim.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,20 +13,35 @@ namespace FoodApp.Models
     {
         public int VendegCimId { get; set; }
         [DisplayName("Vezetéknév:")]
+        [Required]
         public string VezetekNev { get; set; }
+
         [DisplayName("Keresztnév:")]
+        [Required]
         public string KeresztNev { get; set; }
+
         [DisplayName("Város:")]
+        [Required]
         public string Varos { get; set; }
+
         [DisplayName("Irányítószám:")]
+        [Required]
         public int Iranyitoszam { get; set; }
+
         [DisplayName("Utca, házszám:")]
+        [Required]
         public string Cim { get; set; }
+
         [DisplayName("E-mail cím:")]
+        [Required]
         public string Email { get; set; }
+
         [DisplayName("Telefonszám:")]
+        [Required]
         public string Telefonszam { get; set; }
+
         public string UserId { get; set; }
+
         [ForeignKey("UserId")]
         public virtual IdentityUser IdentityUser { get; set; }
     }
