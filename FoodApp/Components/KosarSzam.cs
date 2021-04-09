91a706e9a4b59f2a6da7c4ba129744d5f0b530fe
NagyFelixNF <huntertelo25@gmail.com>
2021-04-09 18:17:55 +0200

    Kosár menü pont szépítve
    Szia Lajos!

---
+++ FoodApp/Components/KosarSzam.cs
@@ -0,0 +1,30 @@
+﻿using FoodApp.Models;
+using FoodApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodApp.Components
+{
+    public class KosarSzam:ViewComponent
+    {
+        private readonly Kocsi _kocsi;
+        public KosarSzam(Kocsi kocsi)
+        {
+            _kocsi = kocsi;
+        }
+        public IViewComponentResult Invoke() 
+        {
+            var items = _kocsi.GetKocsiItems();
+            _kocsi.KocsiItems = items;
+            var KocsiWM = new KocsiViewModel
+            {
+                kocsi = _kocsi,
+                kocsiTotal = _kocsi.GetTotal()
+            };
+            return View(KocsiWM);
+        }
+    }
+}
