ca117adf9ae373c8306397877333758bd7089f2b
TinkaTM <tinka.martin00@gmail.com>
2021-04-19 13:13:07 +0200

    Futár szolgáltatások front-end
    Futár front-end,
    rendelés megerősítés front-end
    meg ilyenek nem tudom elfelejtettem már sajnos

--- FoodApp/Models/FutarAdat.cs
+++ FoodApp/Models/FutarAdat.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -12,16 +13,30 @@ namespace FoodApp.Models
     {
         [Key]
         public int FutarId { get; set; }
+        [Required]
+        [DisplayName("Keresztnév:")]
         public string Keresztnev { get; set; }
+        [Required]
+        [DisplayName("Vezetéknév:")]
         public string Vezeteknev { get; set; }
+        [Required]
+        [DisplayName("Jármű:")]
         public string Jarmu { get; set; }
+        [Required]
+        [DisplayName("Telefonszám:")]
         public string Telefonszam { get; set; }
+        [Required]
+        [DisplayName("Város:")]
         public string Varos { get; set; }
         [DataType(DataType.Time)]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:HH:mm}")]
+        [Required]
+        [DisplayName("Munkaidő kezdete:")]
         public DateTime Kezdes { get; set; }
         [DataType(DataType.Time)]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:HH:mm}")]
+        [Required]
+        [DisplayName("Munkaidő vége:")]
         public DateTime Vegzes { get; set; }
         public string UserId { get; set; }
         [ForeignKey("UserId")]
