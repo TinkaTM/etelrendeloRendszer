4331eadfa6f6fdad6892eec8282c9a0c54ef5218
NagyFelixNF <huntertelo25@gmail.com>
2021-04-29 16:52:57 +0200

    Nem müködő rendelésformat

--- FoodApp/Controllers/EtteremFutarKezelController.cs
+++ FoodApp/Controllers/EtteremFutarKezelController.cs
@@ -1,20 +1,24 @@
 ﻿using FoodApp.Data;
 using FoodApp.ViewModels;
+using FoodApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 
 namespace FoodApp.Controllers
 {
     public class EtteremFutarKezelController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public EtteremFutarKezelController(ApplicationDbContext context)
+        public EtteremFutarKezelController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
         public IActionResult FutarValaszt()
         {
@@ -38,6 +42,44 @@ namespace FoodApp.Controllers
                 vms.Add(vm);
             }
                 
+            return View(vms);
+        }
+        public async Task<IActionResult> FutarRendelesKezel(int Futarid)
+        {
+            var futar = _context.FutarAdat.Find(Futarid);
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            var UserId = user.Id;
+            var Etterem = _context.EtteremCim.Where(ec => ec.UserId == UserId).FirstOrDefault();
+            if (Etterem == null) return View();
+            var etteremid = Etterem.ID;
+            var etelek = _context.RendelesDetail.Where(s => s.EtteremCimId == etteremid).ToList();
+            FutarRendelesKezelViewModel vms = new FutarRendelesKezelViewModel();
+            vms.Rendelesek = new List<RendelesDarab>();
+            vms.Futar = futar;
+            if (etelek.Count > 0)
+            {
+                Dictionary<int, List<RendelesDetail>> dict = new Dictionary<int, List<RendelesDetail>>();
+                foreach (var etel in etelek)
+                {
+                    etel.Etlap = _context.Etlap.Find(etel.EtlapId);
+                    if (!dict.ContainsKey(etel.RendelesId)) dict.Add(etel.RendelesId, new List<RendelesDetail>());
+                    dict[etel.RendelesId].Add(etel);
+                }
+                for (int i = 0; i < 10; i++)
+                {
+                    foreach (var rendelesek in dict)
+                    {
+                        RendelesDarab rd = new RendelesDarab
+                        {
+                            RendelesAdatok = _context.Rendeles.Find(rendelesek.Key),
+                            RendelesEtelek = rendelesek.Value,
+                            Futarnal = false
+
+                        };
+                        vms.Rendelesek.Add(rd);
+                    }
+                }
+            }
             return View(vms);
         }
     }
---
+++ FoodApp/ViewModels/FutarRendelesKezelViewModel.cs
@@ -0,0 +1,20 @@
+﻿using FoodApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodApp.ViewModels
+{
+    public class FutarRendelesKezelViewModel
+    {
+        public FutarAdat Futar { get; set; }
+        public List<RendelesDarab> Rendelesek { get; set; }
+    }
+    public class RendelesDarab
+    {
+        public Rendeles RendelesAdatok { get; set; }
+        public List<RendelesDetail> RendelesEtelek { get; set; }
+        public bool Futarnal { get; set; }
+    }
+}
